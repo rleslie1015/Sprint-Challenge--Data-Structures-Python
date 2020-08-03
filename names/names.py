7a9e8c1a0c5d1a8d9e265bff8fca0d5b44e5d8e5
Leslie Rodriguez <rleslie1015@gmail.com>
2020-08-02 23:45:36 -0500

    complestes names optimization

--- names/names.py
+++ names/names.py
@@ -1,5 +1,5 @@
 import time
-
+from binary_search_tree import BSTNode
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,10 +13,36 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+# solution using BST 
+# first create bst starting with the first name in first list
+bst = BSTNode(names_1[0]) 
+
+# insert all of the name except the name which is already the root node
+for i in range(1,len(names_2)):
+    bst.insert(names_1[i])
+
+# loop through the second list and use the bst contains method to find duplicate names
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
+
+#  runtime varies slightly each time; example runtimes recorded:
+
+# runtime: 0.11457705497741699 seconds
+# runtime: 0.10380697250366211 seconds
+# runtime: 0.13980579376220703 seconds
+# runtime: 0.11453890800476074 seconds
+# runtime: 0.11601901054382324 seconds
+# runtime: 0.11406302452087402 seconds
+# runtime: 0.12554717063903809 seconds
+# runtime: 0.13164591789245605 seconds
+# runtime: 0.11376214027404785 seconds
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
